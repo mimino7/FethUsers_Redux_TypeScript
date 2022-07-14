57fc7e1e09dfb05035bdb642d75e1276a9db7c9e
mimino7 <mimino7@mail.ru>
2022-07-14 13:37:08 +0300

    added_interface_for_action

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,7 +1,18 @@
+  
+  const FETH_USERS = "FETH_USERS";
+  const FETH_USERS_SUCCESS = "FETH_USERS_SUCCESS";
+  const FETH_USERS_ERROR = "FETH_USERS_ERROR";
+  
+  
   interface UserState {
     users: any[];
     loading: boolean;
     error: null | string;
+  }
+
+  interface UserAction {
+    type: string;
+    payload?: any;
   }
   
 const initialState: UserState = {
@@ -10,10 +21,14 @@ const initialState: UserState = {
   error: null,
 };
 
-export const userReducer = (state = initialState, action) => {
+export const userReducer = (state = initialState, action: UserAction): UserState => {
   switch (action.type) {
-    case "FETH_USERS":
-      return { loading: true, error: null, users: [] };
+    case FETH_USERS:
+      return { loading: true, error: null, users: []};
+    case FETH_USERS_SUCCESS:
+      return { loading: false, error: null, users: action.payload};
+    case FETH_USERS_ERROR:
+      return { loading: false, error: action.payload, users: []};
     default:
       return state;
   }
