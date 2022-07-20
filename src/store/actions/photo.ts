0abb94d0cdc683d6b513e34c65cb5fc0dabe7185
mimino7 <mimino7@mail.ru>
2022-07-20 23:26:40 +0300

    added_new-actionCreator_for_pagination

--- src/store/actions/photo.ts
+++ src/store/actions/photo.ts
@@ -22,3 +22,7 @@ export const fetchPhotos = (page = 1, limit = 10) => {
     }
   };
 };
+
+export function setPhotoPage (page: number): PhotosAction {
+  return {type: PhotoActionsTypes.FETCH_PHOTOS_PAGE, payload: page}
+}
